237e76400bc2a619c3c54cdc7e8d5bacdcebdfdd
Starpony999 <csb9@thebrennemans.com>
2026-03-15 21:27:11 -0500

    3/15/26 9:27pm

--- src/main/java/net/starpony/strawberry/effect/DragonsGraceEffect.java
+++ src/main/java/net/starpony/strawberry/effect/DragonsGraceEffect.java
@@ -5,13 +5,8 @@ import net.minecraft.world.effect.MobEffectCategory;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.phys.Vec3;
 
-    public class DragonsGraceEffect extends ModEffects {
-        public DragonsGraceEffect(MobEffectCategory category, int color) {
-            super(category, color);
-        }
-
-        @Override
-        public boolean canApplyUpdateEffect(int duration, int amplifier) {
-            return false;
-        }
+public class DragonsGraceEffect extends MobEffect {
+    public DragonsGraceEffect(MobEffectCategory category, int color) {
+        super(category, color);
     }
+}
